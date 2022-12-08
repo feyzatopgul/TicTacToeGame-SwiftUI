b9f562ce6404f988ad806ea09bccaa306603dd69
Feyza Topgul <feyzatopgul@gmail.com>
2022-12-08 10:35:49 -0800

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by fyz on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
